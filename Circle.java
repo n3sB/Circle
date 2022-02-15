6ecdea0f7eb4b338de6b61fb5e93d235d9bf3e6f
N3sB <enesbaysl@outlook.com>
2022-02-15 23:48:42 +0300

    Circle

---
+++ Circle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Circle {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double pi = 3.14;
+        double diameter, area, circumference;
+
+        System.out.print("Please enter the diameter of the circle: ");
+        diameter = scanner.nextDouble();
+        scanner.close();
+
+        area =pi*(diameter*diameter);
+        circumference = 2*pi*diameter;
+
+        System.out.println("Circumference of the circle: " + circumference);
+        System.out.println(" ");
+        System.out.println("Area of the circle: " + area);
+
+
+
+    }
+}
